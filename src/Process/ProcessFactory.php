6509b16df13fa5074e552667fcb8f80bd19d5c72
Maciej Kobus <maciej.kobus@ez.no>
2020-03-17 14:30:59 +0100

    Refactored creating new Processes via Process::fromShellCommandline

--- src/Process/ProcessFactory.php
+++ src/Process/ProcessFactory.php
@@ -52,7 +52,7 @@ class ProcessFactory
 
     private function createProcess($executeCommand, $arrayEnv)
     {
-        $process = new Process((array)$executeCommand, null, $arrayEnv);
+        $process = Process::fromShellCommandline($executeCommand, null, $arrayEnv);
 
         $process->setTimeout(null);
         // compatibility to SF 2.2
